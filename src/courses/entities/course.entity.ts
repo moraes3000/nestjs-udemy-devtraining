ab39fda2eb179c24f802b87ad83a6df58f65ef57
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-04-26 20:11:44 -0300

    Criando a Entidade Course

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,7 +1,17 @@
 /* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('courses')
 export class Course {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column('json', { nullable: true })
   tag: string[];
 }
